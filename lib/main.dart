87e038368c72d3f9199d2088cdce86453df19952
hikaru.yajima.nz <hikaru.yajima.nz@nttdocomo.com>
2024-11-26 05:47:47 +0000

    textsectionの作成とImageの追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -10,11 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     const String appTitle = 'Flutter layout demo';
     return MaterialApp(
-      title: appTitle,//アプリ内には表示されないタイトル名、webのタブとかで表示される　例：Gmail
+      title: appTitle, //アプリ内には表示されないタイトル名、webのタブとかで表示される　例：Gmail
       home: Scaffold(
         appBar: AppBar(
           title: const Center(
-            child: Text(appTitle),//アプリ内で上部に表示されるタイトル名　例：受信メール
+            child: Text(appTitle), //アプリ内で上部に表示されるタイトル名　例：受信メール
           ),
         ),
         body: const SingleChildScrollView(
@@ -23,8 +22,17 @@ class MyApp extends StatelessWidget {
               TitleSection(
                 name: 'Oeschinen Lake Campground',
                 location: 'Kandersteg, Switzerland',
-                ),
+              ),
               ButtonSection(),
+              TextSection(discription: 
+                'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                'Bernese Alps. Situated 1,578 meters above sea level, it '
+                'is one of the larger Alpine Lakes. A gondola ride from '
+                'Kandersteg, followed by a half-hour walk through pastures '
+                'and pine forest, leads you to the lake, which warms to 20 '
+                'degrees Celsius in the summer. Activities enjoyed here '
+                'include rowing, and riding the summer toboggan run.',
+              )
             ],
           ),
         ),
@@ -46,31 +54,33 @@ class TitleSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(30),//全体の余白
-      
+      padding: const EdgeInsets.all(30), //全体の余白
+
       child: Row(
         children: [
-          Expanded( //Expandedでいい感じの配置になった　余白部分のスペースまで
-            child:
-            Column(
-              mainAxisSize: MainAxisSize.min, 
-              //最小サイズに合わせる
-              crossAxisAlignment: CrossAxisAlignment.start,//左寄せ
-              children: [
-              Text(name,
-                      style: const TextStyle(   //constつけられるのは、変数がなければ
-                      fontWeight: FontWeight.bold,
-                      ),
-              ),
-              Text(location,
-                      style: TextStyle(
-                        color: Colors.grey[500],
-                        )
-              ),
-              ]
-            )
+          Expanded(
+              //Expandedでいい感じの配置になった　余白部分のスペースまで
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  //最小サイズに合わせる
+                  crossAxisAlignment: CrossAxisAlignment.start, //左寄せ
+                  children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    //constつけられるのは、変数がなければ
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(location,
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                    )),
+              ])),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
           ),
-          Icon(Icons.star,color: Colors.red[500],),
           const Text('41'),
         ],
       ),
@@ -115,7 +125,7 @@ class ButtonWithText extends StatelessWidget {
     required this.color,
     required this.icon,
     required this.label,
-    });
+  });
 
   final Color color;
   final IconData icon;
@@ -124,23 +134,42 @@ class ButtonWithText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisSize: MainAxisSize.min,
-
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Padding(
-          padding: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
             ),
           ),
-        ),
-      ]
+        ]);
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.discription,
+  });
+
+  final String discription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(32),
+        child: Text(
+          discription,
+          softWrap: true,//
+        )
     );
   }
 }
+
